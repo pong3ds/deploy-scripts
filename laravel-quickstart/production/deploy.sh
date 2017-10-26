ed29616886f4ecc5991198937b130ccb9ec60bff
Paul Lee <chaiyapong@3dsinteractive.com>
2017-10-26 07:07:28 +0700

    Commit deploy scripts.

---
+++ laravel-quickstart/production/deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+CONFIG_ENV=$1
+SERVER_ENV=$2
+RELEASE_BRANCH=$3
+RELEASE_VERSION=$4
+
+PROJECT_NAME="laravel-quickstart-production"
+SITE_NAME="laravel-quickstart-production.com"
+
+BASE="/bitnami/jenkins/1001/deploy-scripts/base"
+DEPLOY_BASEDIR="/bitnami/jenkins/1001/deploys"
+
+#SOURCE MASTER
+SRC_MASTER_DIR="/var/www/html"
+SRC_MASTER_URL="http://13.229.80.187:8080"
+
+#SERVER
+SERVERS="ubuntu@13.229.124.11"
+SERVER_DOCKER_PATH="/var/dockers/$SITE_NAME"
+SERVER_SOURCE_PATH="/var/www/$SITE_NAME"
+SERVER_PEM="/bitnami/jenkins/1001/pem/laravel-quickstart.pem"
+
+#GITS
+GITREPO_URL="https://$GIT_USER:$GIT_PASS@github.com/pong3ds"
+SOURCE_REPO="$GITREPO_URL/laravel-quickstart-source.git"
+CONFIG_REPO="$GITREPO_URL/laravel-quickstart-config.git"
+DOCKER_REPO="$GITREPO_URL/laravel-quickstart-dockers.git"
+SOURCE_SUBDIR="web"
+
+#BUILD DIR
+DEPLOY_DIR="$DEPLOY_BASEDIR/$PROJECT_NAME"
+DOCKER_DIR="$DEPLOY_DIR/dockers"
+SOURCE_DIR="$DEPLOY_DIR/source"
+CONFIG_DIR="$DEPLOY_DIR/config"
+BUILD_DIR="$DEPLOY_DIR/build"
+FINISH_DIR="$DEPLOY_DIR/finish"
+
+#DOCKER IMAGES
+GIT_IMAGE="3dsinteractive/git-client:1.0"
+COMPOSER_IMAGE="3dsinteractive/composer:7.1"
+
+source /bitnami/jenkins/1001/deploy-scripts/base/deploy-base.sh
+
+run
